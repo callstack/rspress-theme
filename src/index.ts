dc93d1efd9f51fa41473506af20a1fb9f01ab9e2
Jakub Romanczyk <j.romanczyk@gmail.com>
2024-10-19 22:04:43 +0200

    fix: use pure esm in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import path from "node:path";
+import { fileURLToPath } from "node:url";
 import { RspressPlugin } from "@rspress/shared";
+
+const dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export function themePlugin(): RspressPlugin {
   return {
     name: "callstack-rspress-theme",
-    globalStyles: path.join(__dirname, "..", "styles", "theme.css"),
+    globalStyles: path.join(dirname, "..", "styles", "theme.css"),
   };
 }
