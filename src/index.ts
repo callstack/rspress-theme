54755b65d74e37138f4f9a88c391e657ae79de4a
Jakub Romanczyk <j.romanczyk@gmail.com>
2024-10-19 21:36:50 +0200

    refactor: move plugin into index

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-export { callstackThemePlugin } from "./plugin";
+import path from "node:path";
+import { RspressPlugin } from "@rspress/shared";
+
+export function callstackThemePlugin(): RspressPlugin {
+  return {
+    name: "callstack-rspress-theme",
+    globalStyles: path.join(__dirname, "..", "styles", "theme.css"),
+  };
+}
--- src/plugin.ts
+++
@@ -1,9 +0,0 @@
-import path from "node:path";
-import { RspressPlugin } from "@rspress/shared";
-
-export function callstackThemePlugin(): RspressPlugin {
-  return {
-    name: "callstack-rspress-theme",
-    globalStyles: path.join(__dirname, "..", "styles", "theme.css"),
-  };
-}
