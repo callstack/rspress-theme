d487f06548e263537db859a3121b845b2673ad0b
Jakub Romanczyk <j.romanczyk@gmail.com>
2024-10-19 21:57:49 +0200

    chore: add rslib

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import path from "node:path";
 import { RspressPlugin } from "@rspress/shared";
 
-export function callstackThemePlugin(): RspressPlugin {
+export function themePlugin(): RspressPlugin {
   return {
     name: "callstack-rspress-theme",
     globalStyles: path.join(__dirname, "..", "styles", "theme.css"),
