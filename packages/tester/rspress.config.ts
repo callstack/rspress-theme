8cd2afffd41bea0be4a7d77a1f686adc1ccf0654
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2025-07-07 19:35:33 +0200

    feat: overview

--- packages/tester/rspress.config.ts
+++ packages/tester/rspress.config.ts
@@ -10,6 +10,9 @@ export default defineConfig({
     dark: '/rspress-dark-logo.png',
   },
   themeConfig: {
+    overview: {
+      filterNameText: '',
+    },
     socialLinks: [
       {
         icon: 'github',
