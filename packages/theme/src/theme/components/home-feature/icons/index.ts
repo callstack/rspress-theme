e2d92c443b31a2506ffe43c1e2c713f1ec06b484
Jakub Romanczyk <jakub.romanczyk@callstack.com>
2025-07-08 23:56:58 +0200

    feat: icons for home features

---
+++ packages/theme/src/theme/components/home-feature/icons/index.ts
@@ -0,0 +1,6 @@
+export { default as IconServer } from './IconServer';
+export { default as IconLayoutDistributeVertical } from './IconLayoutDistributeVertical';
+export { default as IconSliders2 } from './IconSliders2';
+export { default as IconRepeat } from './IconRepeat';
+export { default as IconLayoutColumns } from './IconLayoutColumns';
+export { default as IconTrendingDown } from './IconTrendingDown';
--- packages/theme/src/theme/components/home-feature/index.tsx
+++ packages/theme/src/theme/components/home-feature/index.tsx
@@ -1,4 +1,12 @@
 import type { Feature, FrontMatterMeta } from '@rspress/shared';
+import {
+  IconLayoutColumns,
+  IconLayoutDistributeVertical,
+  IconRepeat,
+  IconServer,
+  IconSliders2,
+  IconTrendingDown,
+} from './icons';
 import styles from './index.module.scss';
 
 const getGridClass = (feature: Feature): string => {
@@ -19,6 +27,29 @@ const getGridClass = (feature: Feature): string => {
   }
 };
 
+const getIconComponent = (icon: string) => {
+  switch (icon) {
+    case 'server':
+      return IconServer;
+    case 'layout-distribute-vertical':
+      return IconLayoutDistributeVertical;
+    case 'sliders':
+      return IconSliders2;
+    case 'repeat':
+      return IconRepeat;
+    case 'layout-columns':
+      return IconLayoutColumns;
+    case 'trending-down':
+      return IconTrendingDown;
+    default:
+      return icon;
+  }
+};
+
+const isIconComponent = (icon: string | React.ComponentType) => {
+  return typeof icon === 'function';
+};
+
 export function HomeFeature({
   frontmatter,
 }: {
@@ -31,6 +62,7 @@ export function HomeFeature({
     <div className="rp-overflow-hidden rp-m-auto rp-flex rp-flex-wrap rp-max-w-6xl">
       {features?.map((feature) => {
         const { icon, title, details } = feature;
+        const IconComponent = getIconComponent(icon);
 
         return (
           <div
@@ -45,7 +77,11 @@ export function HomeFeature({
                 {icon ? (
                   <div className="rp-flex rp-items-center rp-justify-center">
                     <div className="rspress-home-feature-icon rp-w-12 rp-h-12 rp-text-3xl rp-text-center">
-                      {icon}
+                      {isIconComponent(IconComponent) ? (
+                        <IconComponent />
+                      ) : (
+                        icon
+                      )}
                     </div>
                   </div>
                 ) : null}
